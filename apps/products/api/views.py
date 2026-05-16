dba1bcbd1bf708375b2df34f6d0bb03501e7182c
Tayebe Zare <t.zare1093@gmail.com>
2026-05-16 09:53:35 +0400

    Add product filtering, search, and ordering to API

--- apps/products/api/views.py
+++ apps/products/api/views.py
@@ -1,13 +1,24 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView,CreateAPIView, UpdateAPIView, DestroyAPIView
 from apps.products.models import Product
 from .serializers import ProductSerializer
-from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProductListAPIView(ListAPIView):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = ['brand__brand_title']
+    search_fields = ['product_name', 'description']
+    ordering_fields = ['price', 'id']
 
 
 class ProductDetailAPIView(RetrieveAPIView):
