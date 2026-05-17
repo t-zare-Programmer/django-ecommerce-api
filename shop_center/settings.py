c8fad50136febef79054c4f5893fe0109a455cf1
Tayebe Zare <t.zare1093@gmail.com>
2026-05-17 09:19:08 +0400

    Add pagination to product API

--- shop_center/settings.py
+++ shop_center/settings.py
@@ -185,6 +185,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 from datetime import timedelta
